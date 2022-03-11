67b84f2c833eadbf740ba7a6c8780e7b71817e62
Carlos Wagner <carlos.sperling@granter.com.br>
2022-03-11 10:03:03 -0300

    Teste de comportamentos.

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -66,7 +66,7 @@ namespace NerdStore.Catalogo.Domain
             Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
             Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
             Validacoes.ValidarSeIgual(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
-            Validacoes.ValidarSeMenorIgualMinimo(Valor, 0, "O campo Valor do produto não pode se menor igual a 0");
+            Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode se menor igual a 0");
             Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
         }
     }
